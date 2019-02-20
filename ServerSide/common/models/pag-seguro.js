1b3e5926b7450cbe1a0239ee771ce634e1fb516d
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2019-02-20 13:20:52 -0300

    request

--- ServerSide/common/models/pag-seguro.js
+++ ServerSide/common/models/pag-seguro.js
@@ -1,8 +1,11 @@
 'use strict';
 
+//var http = require("http");
+var request = require('request');
+
 module.exports = function (Pagseguro) {
 
-    idSessao;
+    var idSessao;
 
     /**
      * 
@@ -10,13 +13,19 @@ module.exports = function (Pagseguro) {
      */
 
     Pagseguro.IniciaSessao = function (callback) {
-
+        request.post('https://ws.pagseguro.uol.com.br/v2/sessions?email=paulofore@gmail.com&token=5A0836AD2E5B41B4AD99D' ,{} , (err,response,body) => {
+            console.log('Body:' + body);
+            console.log('Erro:' + err);
+            console.log('Response: ' + response);
+        })
+        /*
         this.http.post(this.urlSession, {}, {}).subscribe(data => {
             console.log('Data:' , data);
             //xml2js.parseString(data["_body"], function (err, result) {
             //  this.idSession = JSON.stringify(result.session.id).replace(/[^a-zA-Z0-9_-]/g, '');
             //});
           });
+        */
         var resposta;
         callback(null, resposta);
     };
