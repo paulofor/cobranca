d064afc66ed0e44ae80b27fe5545ef45d7560c8e
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2019-02-25 13:37:13 -0300

    agora o hash esta chegando

--- ServerSide/common/models/pag-seguro.js
+++ ServerSide/common/models/pag-seguro.js
@@ -6,7 +6,7 @@ var xml2js = require('xml2js');
 
 module.exports = function (Pagseguro) {
 
-    var token = '5A0836AD2E5B41B4AD993D27B4A4ED3D';
+    var token = '';
 
     var urlSession = 'https://ws.pagseguro.uol.com.br/v2/sessions?email=paulofore@gmail.com&token=' + token;
 
